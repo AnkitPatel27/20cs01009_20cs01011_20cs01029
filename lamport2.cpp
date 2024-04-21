436cc55908bcc5cdfbf7f8ac82ab25e36803485b
Rtiik Gupta <ritikgupta9414@gmail.com>
2024-04-21 18:08:48 +0530

    logs

--- lamport2.cpp
+++ lamport2.cpp
@@ -10,8 +10,9 @@
 #include <csignal>
 #include <algorithm>
 #include <queue>
+#include <fstream> 
 #include <semaphore.h>
-#include <thread> 
+#include <thread>
 #include <ctime>
 using namespace std;
 
@@ -19,308 +20,342 @@ using namespace std;
 #define PORT1 8082
 #define PORT2 8081
 #define PORT3 8083
+// No of requests
 #define callLimit 1
 int PID = 2;
-sem_t lockClock,mutex;
+sem_t lockClock, mutex;
 int LamportClock = 0;
-vector <int> allSockID;
-priority_queue <pair<int,int>,vector <pair<int,int>>,greater<pair<int,int>>> requestQueue; //timestamp, PID
-// make a binary semaphore  
+vector<int> allSockID;
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> requestQueue; // timestamp, PID
+// make a binary semaphore
 
-
-struct data_thread {
+struct data_thread
+{
     int th_id;
     int timestamp;
 };
 
-void signal_handler(int signal) {
+void signal_handler(int signal)
+{
     std::cout << "Signal received. Closing socket." << std::endl;
-    for(auto i : allSockID){
-        if(i>=100){
-            close(i-100);
+    for (auto i : allSockID)
+    {
+        if (i >= 100)
+        {
+            close(i - 100);
         }
-        else{
+        else
+        {
             close(i);
         }
     }
-    exit(signal); 
+    exit(signal);
 }
 
-void incrementLogicalClock(){
+void incrementLogicalClock()
+{
     sem_wait(&lockClock);
     LamportClock++;
     sem_post(&lockClock);
 }
 
-void pushIntoQueue(int PID,int timestamp,bool fromClient){
+void pushIntoQueue(int PID, int timestamp, bool fromClient)
+{
     sem_wait(&mutex);
-    if(fromClient){
-        cout<<"from client pushed "<<PID<<" "<<timestamp<<endl;
-    }
-    else{
-        cout<<"from server pushed "<<PID<<" "<<timestamp<<endl;
-    }
-    requestQueue.push({timestamp,PID});
+    // if(fromClient){
+    //     cout<<"from client pushed "<<PID<<" "<<timestamp<<endl;
+    // }
+    // else{
+    //     cout<<"from server pushed "<<PID<<" "<<timestamp<<endl;
+    // }
+    requestQueue.push({timestamp, PID});
     sem_post(&mutex);
 }
 
-void popFromQueue(){
+void popFromQueue()
+{
     sem_wait(&mutex);
-    if(requestQueue.size()>0){
+    if (requestQueue.size() > 0)
+    {
         requestQueue.pop();
     }
     sem_post(&mutex);
 }
 
-
-bool checkTurn(){
+bool checkTurn()
+{
     sem_wait(&mutex);
-    bool ans = (requestQueue.top().second==PID);
+    bool ans = (requestQueue.top().second == PID);
     sem_post(&mutex);
     return ans;
 }
 
-void runCriticalSection(){
+void runCriticalSection()
+{
     incrementLogicalClock();
-    cout<<"Critical Section\n";
+    cout << "Inside Critical Section---------------------------\n";
+    cout << "Writing in file";
+    // Make a file and write it
+    std::ofstream outfile("critical_section.txt");
+
+    outfile << "my text here!" << std::endl;
+
+    outfile.close();
     sleep(5);
-    cout<<"Critical Section End\n";
+    cout << "Critical Section End---------------------------\n";
     popFromQueue();
 }
 
-void parsePacket(const char* input, int* timestamp, int* pid, char* data) {
-    char* token;
-    char* rest = (char*)input;
+void parsePacket(const char *input, int *timestamp, int *pid, char *data)
+{
+    char *token;
+    char *rest = (char *)input;
 
     // Extract the timestamp
     token = strtok_r(rest, " ", &rest);
-    if (token != NULL) {
+    if (token != NULL)
+    {
         *timestamp = atoi(token);
     }
 
     // Extract the PID
     token = strtok_r(NULL, " ", &rest);
-    if (token != NULL) {
+    if (token != NULL)
+    {
         *pid = atoi(token);
     }
 
     // Extract the data
-    if (rest != NULL) {
+    if (rest != NULL)
+    {
         strcpy(data, rest);
     }
 }
 
-int sendEvent(int clientSocket,string sendMsg){
+int sendEvent(int clientSocket, string sendMsg)
+{
     incrementLogicalClock();
-    string msg = to_string(LamportClock) + " " + to_string(PID)+" "+ sendMsg;
-    const char* message = msg.c_str();
-    int a = send(clientSocket, message, strlen(message), 0); 
+    string msg = to_string(LamportClock) + " " + to_string(PID) + " " + sendMsg;
+    const char *message = msg.c_str();
+    int a = send(clientSocket, message, strlen(message), 0);
     return a;
 }
 
-pair<string,pair<int,int>> recvEvent(int clientSocket){
-    char buffer[1024] = { 0 };
+pair<string, pair<int, int>> recvEvent(int clientSocket)
+{
+    char buffer[1024] = {0};
     recv(clientSocket, buffer, sizeof(buffer), 0);
-    cout<<buffer<<endl;
+    // cout<<buffer<<endl;
     int timestamp;
     int rpid;
     char data[1024];
     parsePacket(buffer, &timestamp, &rpid, data);
-    
+
     sem_wait(&lockClock);
-    LamportClock = max(LamportClock,timestamp)+1;
+    LamportClock = max(LamportClock, timestamp) + 1;
     sem_post(&lockClock);
 
     string str(data, strlen(data));
-    return {str ,{rpid,timestamp}}; 
+    return {str, {rpid, timestamp}};
 }
 
-void printQueue(){
+void printQueue()
+{
     sem_wait(&mutex);
-    cout<<"Queue\n";
-    priority_queue <pair<int,int>,vector <pair<int,int>>,greater<pair<int,int>>>  temp(requestQueue);
-    
-    while(!temp.empty()){
-        cout<<temp.top().first<<" "<<temp.top().second<<endl;
+    cout << "Printing Queue:- \n";
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> temp(requestQueue);
+
+    while (!temp.empty())
+    {
+        cout << temp.top().first << " " << temp.top().second << endl;
         temp.pop();
     }
     sem_post(&mutex);
-
 }
 
-void* ServerThread(void* arg) {
-    
-    // creating socket 
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0); 
+void *ServerThread(void *arg)
+{
+
+    // creating socket
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     allSockID.push_back(serverSocket);
 
-    // specifying the address 
-    sockaddr_in serverAddress; 
-    serverAddress.sin_family = AF_INET; 
-    serverAddress.sin_port = htons(PORT1); 
-    serverAddress.sin_addr.s_addr = INADDR_ANY; 
-  
-    // binding socket. 
-    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)); 
-  
-    // listening to the assigned socket 
-    listen(serverSocket, 5); 
-    cout<<"server listening\n";
-    // accepting connection request 
-    while(1){
-        int clientSocket = accept(serverSocket, nullptr, nullptr); 
-        // recieving data 
-        pair<string,pair<int,int>> data = recvEvent(clientSocket);
-        
-        if("request_Resource"==data.first){
-            pushIntoQueue(data.second.first,data.second.second,false);
-            sendEvent(clientSocket,"reply_ok");
-            cout<<"replied\n";
-        
+    // specifying the address
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(PORT1);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    // binding socket.
+    bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+
+    // listening to the assigned socket
+    listen(serverSocket, 5);
+    cout << "Server listening\n";
+    // accepting connection request
+    while (1)
+    {
+        int clientSocket = accept(serverSocket, nullptr, nullptr);
+        // recieving data
+        pair<string, pair<int, int>> data = recvEvent(clientSocket);
+
+        if ("request_Resource" == data.first)
+        {
+            pushIntoQueue(data.second.first, data.second.second, false);
+            sendEvent(clientSocket, "reply_ok");
+            cout << "Replied\n";
         }
-        else if("pop_from_queue"==data.first){
+        else if ("pop_from_queue" == data.first)
+        {
             popFromQueue();
-            cout<<"popped from queue\n";
+            cout << "Popped from queue\n";
         }
     }
 
-
-    // closing the socket. 
+    // closing the socket.
     close(serverSocket);
     allSockID.erase(find(allSockID.begin(), allSockID.end(), serverSocket));
-    cout<<"closed server\n";
+    cout << "Closed server\n";
     pthread_exit(NULL);
 }
 
-void releaseResource(int PORT){
-    int clientSocket = socket(AF_INET, SOCK_STREAM, 0); 
-    allSockID.push_back(100+clientSocket);
+void releaseResource(int PORT)
+{
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    allSockID.push_back(100 + clientSocket);
 
-    sockaddr_in serverAddress; 
-    serverAddress.sin_family = AF_INET; 
-    serverAddress.sin_port = htons(PORT); 
-    serverAddress.sin_addr.s_addr = INADDR_ANY; 
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(PORT);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-
-    int a = connect(clientSocket, (struct sockaddr*)&serverAddress,  sizeof(serverAddress)); 
+    int a = connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
     // cout<<a<<endl;
-    // sending data 
+    // sending data
 
-    while(1){
-        int a = sendEvent(clientSocket,"pop_from_queue");
-        cout<<"Send "<<a<<endl;
-        if(a>0){
+    while (1)
+    {
+        int a = sendEvent(clientSocket, "pop_from_queue");
+        cout << "Releasing Resourse from process: " << PID << endl;
+        if (a > 0)
+        {
             break;
         }
     }
 
     close(clientSocket);
-    allSockID.erase(find(allSockID.begin(), allSockID.end(), clientSocket+100));
+    allSockID.erase(find(allSockID.begin(), allSockID.end(), clientSocket + 100));
 }
 
-void requestResource(int PORT){
-     int clientSocket = socket(AF_INET, SOCK_STREAM, 0); 
-        cout<<"client socket "<<clientSocket;
+void requestResource(int PORT)
+{
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    // cout<<"client socket "<<clientSocket;
 
-        allSockID.push_back(100+clientSocket);
-        
+    allSockID.push_back(100 + clientSocket);
 
-        // specifying address 
-        sockaddr_in serverAddress; 
-        serverAddress.sin_family = AF_INET; 
-        serverAddress.sin_port = htons(PORT); 
-        serverAddress.sin_addr.s_addr = INADDR_ANY; 
-    
+    // specifying address
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(PORT);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-        int a = connect(clientSocket, (struct sockaddr*)&serverAddress,  sizeof(serverAddress)); 
+    int a = connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
 
-        string msg = to_string(LamportClock) + " " + to_string(PID) + " request_Resource";
-        const char* message = msg.c_str();
+    string msg = to_string(LamportClock) + " " + to_string(PID) + " request_Resource";
+    const char *message = msg.c_str();
 
-        while(1){
-            int a = sendEvent(clientSocket,"request_Resource");
-            cout<<"Send "<<a<<endl;
-            pair<string,pair<int,int>> data = recvEvent(clientSocket);
-            if("reply_ok"==data.first){
-                cout<<"got reply"<<PORT<<"\n";
-                break;
-            }
+    while (1)
+    {
+        int a = sendEvent(clientSocket, "request_Resource");
+        cout << "Send " << a << endl;
+        pair<string, pair<int, int>> data = recvEvent(clientSocket);
+        if ("reply_ok" == data.first)
+        {
+            cout << "got reply" << PORT << "\n";
+            break;
         }
-        
-        cout<<"lampart clock "<<LamportClock<<"\n";
-        // closing socket 
-        close(clientSocket);
-        allSockID.erase(find(allSockID.begin(), allSockID.end(), clientSocket+100));
+    }
 
+    // cout<<"lampart clock "<<LamportClock<<"\n";
+    // closing socket
+    close(clientSocket);
+    allSockID.erase(find(allSockID.begin(), allSockID.end(), clientSocket + 100));
 }
 
-void* ClientThread(void* arg) {
-    
+void *ClientThread(void *arg)
+{
+
     srand(std::time(0));
-    for(int i=0;i<callLimit;i++){
-        int x = 2+rand()%10;
-        //int x = 5;
-        cout<<"rand()%10 "<<x<<endl;
+    for (int i = 0; i < callLimit; i++)
+    {
+        int x = 2 + rand() % 5;
+        // int x = 5;
+        cout << "Waiting for " << x << " seconds to client thread!!!" << endl;
         sleep(x);
-        cout<<"client thread\n";
-        pushIntoQueue(PID,LamportClock,true);
+        // cout<<"client thread\n";
+        pushIntoQueue(PID, LamportClock, true);
         requestResource(PORT2);
         requestResource(PORT3);
 
-        cout<<"lampart clock "<<LamportClock<<"\n";
-        
-        while(!checkTurn()){
+        cout << "Current lamport's clock time: " << LamportClock << "\n";
+
+        while (!checkTurn())
+        {
             continue;
         }
         printQueue();
         runCriticalSection();
         releaseResource(PORT2);
         releaseResource(PORT3);
-        //socket 
+        // socket
 
-        cout<<"Thread exited\n";
+        cout << "Client Thread exited\n";
     }
-    
+
     pthread_exit(NULL);
 }
 
-int main() {
-    sem_init(&mutex, 0, 1); 
-    sem_init(&lockClock, 0, 1); 
+int main()
+{
+    sem_init(&mutex, 0, 1);
+    sem_init(&lockClock, 0, 1);
 
     pthread_t threads[NUM_TH];
     struct data_thread td[NUM_TH];
     int rc;
     int i;
-    
+
     // cout<<"Enter PID";
     // cin>>PID;
 
     signal(SIGINT, signal_handler);
     signal(SIGTERM, signal_handler);
 
-
-    for (i = 0; i < NUM_TH; i++) {
-        cout << "creating thread " << i << endl;
+    for (i = 0; i < NUM_TH; i++)
+    {
+        cout << "Creating thread: " << i << endl;
         td[i].th_id = i;
         td[i].timestamp = i;
 
-        if(i==0){
-            rc = pthread_create(&threads[i], NULL, ServerThread, (void*)&td[i]);
+        if (i == 0)
+        {
+            rc = pthread_create(&threads[i], NULL, ServerThread, (void *)&td[i]);
         }
-        else{
-            rc = pthread_create(&threads[i], NULL, ClientThread, (void*)&td[i]);
+        else
+        {
+            rc = pthread_create(&threads[i], NULL, ClientThread, (void *)&td[i]);
         }
 
-        if (rc) {
+        if (rc)
+        {
             cout << "Error: unable to create thread," << rc << endl;
             exit(-1);
         }
     }
 
-
-
-    for (i = 0; i < NUM_TH; i++) {
+    for (i = 0; i < NUM_TH; i++)
+    {
         pthread_join(threads[i], NULL);
     }
 
